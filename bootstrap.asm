2990a09e5b1eb92cdfedac243ff3614041a52520
Gus Sánchez <goosethedev@proton.me>
2026-05-02 22:58:28 -0400

    initial offset and cleaner setup

--- bootstrap.asm
+++ bootstrap.asm
@@ -1,21 +1,28 @@
-start:
-    ; load bootloader at known BIOS address 0x07C00
-    ; physical addr (real-mode) = segment * 16 + offset
-    mov ax, 0x07C0
-    mov ds, ax
+[bits 16]       ; emit 16-bit code (real-mode)
+[org 0x7C00]    ; offset all addresses to this starting point
 
-    ; welcome string
+start:
+    ; setup a clean environment
+    cli                 ; disable BIOS interruptions while setting up
+    xor ax, ax          ; zero-out ax
+    mov ds, ax          ; clear data segment
+    mov es, ax          ; clear extra segment
+    mov ss, ax          ; clear stack segment
+    mov sp, 0x7C00      ; grow stack pointer below our code (sp decreases)
+    sti                 ; re-enable interrupts
+
+    ; print bootloader welcome string
     mov si, title_str
     call print_str
 
     ; load kernel and execute it
     call load_kernel_from_disk
-    jmp 0x0900:0000
+    jmp 0x0500:0000
 
 
-; load the kernel at 0x9000 (div by 16 for physical addr)
+; load the kernel at 0x0500 (small but ok for this stage)
 load_kernel_from_disk:
-    mov ax, 0x0900
+    mov ax, 0x0500
     mov es, ax
 
     ; execute BIOS service 13h:02h (read from hard-disk)
@@ -23,7 +30,7 @@ load_kernel_from_disk:
     mov al, 01h     ; read only one sector
     mov ch, 0h      ; hdd track we want to read (track 0)
     mov cl, 02h     ; sector to read (second)
-    mov dh, 0h      ; head number (?)
+    mov dh, 0h      ; head number (platter head on old hard-disks)
     mov dl, 80h     ; type of disk to read (0h=floppy, 80h=hdd0)
     mov bx, 0h      ; offset to the starting point (zero)
     int 13h         ; bios hard-disk service category
@@ -64,7 +71,7 @@ print_finished:
 
     ret
 
-title_str       db "BOOTLOADER: loading 539 kernel...", 0
+title_str       db "BOOTLOADER: loading 539kernel...", 0
 load_error_str  db "BOOTLOADER: ERROR! the kernel failed to load", 0
 
 ; pad with zeros until addr 510
